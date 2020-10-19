88641f082d3e42a88fe0d203fd81cadc2cbee47c
zoeng <zoe.ng@utexas.edu>
2020-10-19 16:44:09 -0500

    init commit

---
+++ SinglePhoto/SinglePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by Zoe Ng on 10/2/20.
+//  Copyright © 2020 Zoe Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
